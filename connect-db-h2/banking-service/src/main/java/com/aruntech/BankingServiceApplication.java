f9cae8e6976762e7294b5593bfbcec80f0f80f85
ArunStephen <arunstephen.techarch@gmail.com>
2022-10-16 23:37:45 +0530

    h2db interaction commit

---
+++ connect-db-h2/banking-service/src/main/java/com/aruntech/BankingServiceApplication.java
@@ -0,0 +1,18 @@
+package com.aruntech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+public class BankingServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BankingServiceApplication.class, args);
+    }
+
+}
