d6a756459270c736b8f3da5ca38b4fbe05165115
ArunStephen <arunstephen.techarch@gmail.com>
2022-10-17 17:10:42 +0530

    updated configuration

--- connect-db-h2/banking-service/src/main/java/com/aruntech/BankingServiceApplication.java
+++ connect-db-h2/banking-service/src/main/java/com/aruntech/BankingServiceApplication.java
@@ -2,8 +2,6 @@ package com.aruntech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.context.annotation.Bean;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
