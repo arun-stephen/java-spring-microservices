834614c4db7d7fd3df412613469be2cd44ac553c
ArunStephen <arunstephen.techarch@gmail.com>
2022-10-28 00:55:35 +0530

    updated code

--- service-discovery/banking-service/src/main/java/com/aruntech/BankingServiceApplication.java
+++ service-discovery/banking-service/src/main/java/com/aruntech/BankingServiceApplication.java
@@ -2,10 +2,10 @@ package com.aruntech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class BankingServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(BankingServiceApplication.class, args);
